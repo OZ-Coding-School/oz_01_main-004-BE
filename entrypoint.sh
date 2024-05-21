77c52407f261e90d1280afca639c91b17ba2a6a3
SwanyCastle <seunghwan712@gmail.com>
2024-05-21 11:45:10 +0900

    Deploy: 서버 배포후 websocket daphne 연결 설정
    nginx 설정파일 수정 및 docker-compose.yml , entrypoint.sh 파일 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Collect static files
-#echo "Collect static files"
-#python manage.py collectstatic
+#echo "Collecting static files"
+#python manage.py collectstatic --noinput
 
 # Apply database migrations
-#echo "Apply database migrations"
-#python manage.py makemigrations
-#python manage.py migrate
+echo "Applying database migrations"
+python manage.py makemigrations
+python manage.py migrate
 
-# Start Gunicorn
-echo "Starting Gunicorn"
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4
+# Start Gunicorn or Daphne based on the command passed in
+exec "$@"
