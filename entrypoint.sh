e26e5d5a5ec33f59fc24cf7e28c20f13652c06ae
SwanyCastle <seunghwan712@gmail.com>
2024-05-23 13:42:23 +0900

    Fix: entrypoint.sh 수정
    entrypoint.sh 내의 migrations 명령 주석처리

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@
 #python manage.py collectstatic --noinput
 
 # Apply database migrations
-echo "Applying database migrations"
-python manage.py makemigrations
-python manage.py migrate
+#echo "Applying database migrations"
+#python manage.py makemigrations
+#python manage.py migrate
 
 # Start Gunicorn or Daphne based on the command passed in
 exec "$@"
