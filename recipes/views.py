b2a379515480f702982c2210871ccf8d4df7af6d
SwanyCastle <seunghwan712@gmail.com>
2024-05-24 16:35:01 +0900

    Fix: 페이지네이션 버그 수정

--- recipes/views.py
+++ recipes/views.py
@@ -54,18 +54,23 @@ class RecipeListAPIView(APIView):
     @staticmethod
     def get_list(request):
         recipes = Recipe.objects.all().order_by("-created_at")
+
         search = request.query_params.get("search")
         if search:
             recipes = recipes.filter(Q(title__icontains=search) | Q(content__icontains=search))
+
         food_type = request.query_params.get("food_type")
         if food_type:
             recipes = recipes.filter(food_type=food_type)
+
         food_ingredient = request.query_params.get("food_ingredient")
         if food_ingredient:
             recipes = recipes.filter(food_ingredient=food_ingredient)
+
         level = request.query_params.get("level")
         if level:
             recipes = recipes.filter(level=level)
+
         return recipes
 
     def get(self, request):
@@ -79,20 +84,18 @@ class RecipeListAPIView(APIView):
                 status=status.HTTP_200_OK,
             )
         serializer = RecipeSerializer(page, many=True, context={"request": request})
-        return Response(
-            {
-                "message": "Successfully Read Recipe List",
-                "recipe_list": serializer.data,
-                "count": paginator.page.paginator.count,
-                "next": paginator.get_next_link(),
-                "previous": paginator.get_previous_link(),
-            }
-        )
+        return paginator.get_paginated_response(serializer.data)
+        # return Response(
+        #     {
+        #         "message": "Successfully Read Recipe List",
+        #         "recipe_list": serializer.data,
+        #         "count": paginator.page.paginator.count,
+        #         "next": paginator.get_next_link(),
+        #         "previous": paginator.get_previous_link(),
+        #     }
+        # )
 
     def post(self, request):
-        user = request.user
-        if not user.is_authenticated:
-            return Response(data={"message": "해당 유저 정보를 찾을 수 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
         serializer = self.serializer_class(data=request.data, context={"request": request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -143,7 +146,8 @@ class RecipeDetailAPIView(APIView):
         )
 
     def put(self, request, recipe_id):
-        recipe = Recipe.objects.filter(pk=recipe_id).first()
+        user = request.user
+        recipe = Recipe.objects.filter(pk=recipe_id, user_id=user.id).first()
         serializer = self.serializer_class(recipe, data=request.data, context={"request": request}, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -152,7 +156,8 @@ class RecipeDetailAPIView(APIView):
         )
 
     def delete(self, request, recipe_id):
-        recipe = Recipe.objects.filter(pk=recipe_id).first()
+        user = request.user
+        recipe = Recipe.objects.filter(pk=recipe_id, user_id=user.id).first()
         recipe.delete()
         return Response(data={"message": "Successfully Deleted Recipe."}, status=status.HTTP_200_OK)
 
@@ -161,7 +166,6 @@ class RecipeImageAPIView(APIView):
     serializer_class = RecipeImageSerializer
 
     def post(self, request):
-        # serializer = self.serializer_class(data=request.data, context={"recipe_id": recipe_id})
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
